3a4142a1c8a9a536f730a2fdaafde18d8ae222b8
Davide Gerosa <d.gerosa@damtp.cam.ac.uk>
2016-02-04 09:49:05 +0000

    v 33

--- precession/__init__.py
+++ precession/__init__.py
@@ -112,7 +112,7 @@ __author__ = "Davide Gerosa"
 __email__ = "d.gerosa@damtp.cam.ac.uk"
 __copyright__ = "Copyright (C) 2015 Davide Gerosa"
 __license__ = "CC by-nc-sa 3.0"
-__version__ = "0.0.0.32"
+__version__ = "0.0.0.33"
 
 
 __doc__="**Author** "+__author__+"\n\n"+\
