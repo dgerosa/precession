746d91a1d36ae0b1c872fabbdfc39a99881049e2
Davide Gerosa <d.gerosa@damtp.cam.ac.uk>
2016-02-04 09:44:50 +0000

    push documentation on gh-pages branch

--- generate_documentation.sh
+++ generate_documentation.sh
@@ -2,6 +2,19 @@
 # pdoc is available here https://github.com/BurntSushi/pdoc
 # and can be installed from Pypi: pip install pdoc
 
+#python <<END
+#import precession
+#print "Generating documentation of precession, version", precession.__version__
+#END
+
+#pdoc --html --overwrite precession
+#rm precession/*pyc precession/*/*pyc
+
+
+##################
+
+git checkout master
+
 python <<END
 import precession
 print "Generating documentation of precession, version", precession.__version__
@@ -9,3 +22,27 @@ END
 
 pdoc --html --overwrite precession
 rm precession/*pyc precession/*/*pyc
+
+temp1=`mktemp`
+cp precession/index.html $temp1
+temp2=`mktemp`
+cp precession/test/index.html $temp2
+
+
+git add *
+git commit -m "generate_documentation.sh"
+git push
+
+git checkout gh-pages
+mv $temp index.html
+mv $temp2 test/index.html
+
+git add *
+git commit -m "generate_documentation.sh"
+
+rm $temp1 $temp2
+
+git checkout master
+
+
+
