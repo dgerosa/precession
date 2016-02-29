82b3ca0ba4dbbd34d8cc1226802252e9f34c196d
Davide Gerosa <d.gerosa@damtp.cam.ac.uk>
2016-02-29 15:53:59 +0000

    markdown readme

--- setup.py
+++ setup.py
@@ -5,24 +5,22 @@ To publish on Pypi:
 '''
 
 from setuptools import setup
+from pypandoc import convert
+    
+read_md = lambda f: convert(f, 'rst') # Convert markdown to rst for pypi
 
-# Fill "long_description" using the README file
-def readme():
-    with open('README.rst') as f:
-        return f.read()
-        
 # Extract code version from __init__.py 
 def get_version():
     with open('precession/__init__.py') as f:
         for line in f.readlines():
-            if "version" in line:
+            if "__version__" in line:
                 return line.split('"')[1]
 
 setup(
     name='precession',
     version=get_version(),
     description='Dynamics of precessing black-hole binaries',
-    long_description=readme(),
+    long_description=read_md('README.md'),
     classifiers=[
         'Topic :: Scientific/Engineering :: Astronomy',
         'Topic :: Scientific/Engineering :: Physics',
@@ -32,7 +30,7 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     keywords='black hole spin inspiral precession post-Newtonian',
-    url='http://davidegerosa.com/spinprecession/',
+    url='https://github.com/dgerosa/precession/',
     author='Davide Gerosa',
     author_email='d.gerosa@damtp.cam.ac.uk',
     license='CC by-nc-sa 3.0',
