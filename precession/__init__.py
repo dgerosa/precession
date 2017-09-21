c06ad5eb0f4ef8644ab5f928be7e81cda9524724
Davide Gerosa <dgerosa@caltech.edu>
2017-09-21 09:51:55 -0700

    make hidden variables available

--- precession/__init__.py
+++ precession/__init__.py
@@ -1,1 +1,7 @@
 from precession import *
+__author__ = precession.__author__
+__email__ = precession.__email__
+__copyright__ = precession.__copyright__
+__license__ = precession.__license__
+__version__ = precession.__version__
+__doc__ = precession.__doc__
