4a7175edb0cc11b35ad8308ca95ade0bf03836f4
Davide Gerosa <dgerosa@star.sr.bham.ac.uk>
2020-05-27 10:11:12 +0100

    wiping for v2

--- precession/__init__.py
+++
@@ -1,8 +0,0 @@
-from .precession import __author__
-from .precession import __email__
-from .precession import __copyright__
-from .precession import __license__
-from .precession import __version__
-from .precession import __doc__
-
-from .precession import *
