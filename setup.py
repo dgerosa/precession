4417eadcedbbc65e4c98c783cdb8aff81f311914
MDM988 <mmould@star.sr.bham.ac.uk>
2020-05-27 14:58:00 +0100

    some more files

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+import setuptools
+
+about = {}
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'precession', '__version__.py'), 'r') as _:
+    exec(_.read(), about)
+
+with open ('README.md', 'r') as _:
+    readme = _.read()
+
+with open ('requirements.txt', 'r') as _:
+    requires = [line.split()[0] for line in _]
+
+setuptools.setup(
+    name=about['__title__'],
+    description=about['__description__'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    version=about['__version__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    project_urls={
+        'Source Code': about['__url__'],
+        'Documentation': 'https://dgerosa.github.io/precession/'
+    },
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires='>=3.5.*',
+    install_requires=requires,
+    license=about['__license__']
+)
