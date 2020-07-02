adf672ea41d2b2d5f05a51669f5b502eb4b3082e
MDM988 <mmould@star.sr.bham.ac.uk>
2020-07-02 15:17:59 +0100

    remove unnecessary

--- precession/precession.py
+++ precession/precession.py
@@ -4,8 +4,6 @@ precession
 
 import numpy as np
 import scipy, scipy.special, scipy.integrate
-# Using precession_v1 functions for now
-import precession as pre
 import sys, os, time
 import warnings
 import itertools
@@ -2867,91 +2865,6 @@ def omega2_updown(r, q, chi1, chi2):
     return omega2
 
 
-
-
-
-
-
-#def newlen(var):
-#    """Redefine len function
-#    """
-#
-#    try:
-#        n = len(var)
-#    except:
-#        n = 1
-#    return n
-
-
-class Binary:
-    """Test class of a binary black hole
-
-    Parameters
-    ----------
-    q: float
-        Mass ratio m2/m1, 0 <= q <= 1.
-
-    chi1: float
-        Kerr parameter of the heavier black hole, 0 <= chi1 <= 1.
-
-    chi2: float
-        Kerr parameter of the lighter black hole, 0 <= chi2 <= 1.
-    """
-
-    # How should we represent a single binary?
-    # The fixed parameters without initialization are
-    # M, q, chi1, chi2 (or m1, m2, S1, S2)
-    # To initialize a binary we also require
-    # r (or L) and xi, J, S (or theta1, theta2 and DeltaPhi)
-    # Different parameters are required for different evolutions:
-    # finite/infinite precession-averaged or orbit-averaged
-    # For the precession-averaged we can include finite and infinite in one
-    # and allow for r = np.inf with a flag to check for this
-    # Should we ignore the total mass M?
-    # Or include it everywhere as an optional parameter?
-
-
-    def __init__(self, q, chi1, chi2):
-
-        assert q >= 0. and q <= 1.
-        assert chi1 >= 0. and chi1 <= 1.
-        assert chi2 >= 0. and chi2 <= 1.
-
-        self.q = q
-        self.chi1 = chi1
-        self.chi2 = chi2
-        self.M = 1.
-        self.m1 = self.M / (1. + self.q)
-        self.m2 = self.m1 * self.q
-        self.S1 = self.chi1 * self.m1**2
-        self.S2 = self.chi2 * self.m2**2
-
-    def config_angles(self, ri, theta1, theta2, deltaphi):
-        """
-        """
-
-        xii, Ji, Si = pre.from_the_angles(theta1, theta2, deltaphi, self.q,
-                                          self.S1, self.S2, ri)
-        self.ri = ri
-        self.xii = xii
-        self.Ji = Ji
-        self.Si = Si
-
-        return xii, Ji, Si
-
-    def orb_evolve(self, r):
-        """
-        """
-
-        assert r[0] == self.ri
-        J, xi, S = pre.orbit_averaged(self.Ji, self.xii, self.Si, r, self.q,
-                                      self.S1, self.S2)
-        self.r = r
-        self.xi = xi
-        self.J = J
-        self.S = S
-
-        return xi, J, S
 
 
 if __name__ == '__main__':
