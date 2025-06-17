465fad9043ca847496cff13ec8467ac6bf9ef365
Giulia Fumagalli <g.fumagalli47@campus.unimib.it>
2025-06-17 11:46:08 +0200

    error in __init__.py file
    Sorry, I forgot to comment out a line, and the dependencies were not correct. Now everything should work.
    I also checked that precession.eccentricity. ispiral_orbav  with e=0 returns the same results as the circular case when the spin terms are tuned off.

--- precession/__init__.py
+++ precession/__init__.py
@@ -1,10 +1,12 @@
 from .__version__ import *
 from .precession import *
-from .eccentricity import *
-
+#from precession.eccentricity import *
+"""
 __all__ = []
 try:
-    from .eccentricity import __all__ as module_all
+    from precession.eccentricity import __all__ as module_all
     __all__.extend(module_all)
 except ImportError:
     pass
+
+"""
